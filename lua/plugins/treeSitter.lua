14e55a518b873a18e23cb928651a03724442f938
vine <117782568+vinegm@users.noreply.github.com>
2026-06-26 09:37:44 -0300

    Tree-sitter default langs

--- lua/plugins/treeSitter.lua
+++ lua/plugins/treeSitter.lua
@@ -3,6 +3,26 @@ return { -- Highlight, edit, and navigate code
   lazy = false,
   build = ":TSUpdate",
   config = function()
+    require("nvim-treesitter").install({
+      -- Languages Related
+      "bash",
+      "c",
+      "cpp",
+      "go",
+      "gomod",
+      "gosum",
+      "lua",
+      "python",
+      "vim",
+      "vimdoc",
+
+      -- General Formats
+      "dockerfile",
+      "json",
+      "markdown",
+      "yaml",
+    })
+
     local group = vim.api.nvim_create_augroup("treesitter-start", { clear = true })
     vim.api.nvim_create_autocmd("FileType", {
       group = group,
